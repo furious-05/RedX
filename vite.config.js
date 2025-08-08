7ec25389db5faf35ef2809cde8b7345841ff0afb
mun1bxD <muneebnawaz3849@gmail.com>
2025-08-08 17:10:59 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pentestX/", // <-- Add this line with your repo name
   plugins: [tailwindcss(), react()],
 });
